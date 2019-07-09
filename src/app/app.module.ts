8b940950fa5310484e3c37a33281301cfbae3853
Josh <ofgodsandmythos@gmail.com>
2019-07-09 13:41:21 -0500

    Setup animation for the Glyphs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RuneScapeComponent } from './components/rune-scape/rune-scape.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { RuneScapeComponent } from './components/rune-scape/rune-scape.component
     RuneScapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/rune-scape/rune-scape.component.ts
+++ src/app/components/rune-scape/rune-scape.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {PRIME_RUNES, Rune, RUNE_SETS} from '../../domain/rune';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'mmr-rune-scape',
   templateUrl: './rune-scape.component.html',
-  styleUrls: ['./rune-scape.component.css']
+  styleUrls: ['./rune-scape.component.css'],
+  animations: [
+    trigger('enteredExited', [
+      state('entered', style({
+        height: '70px',
+        width: '70px',
+        'font-size': '24pt'
+      })),
+      state('exited', style({
+        height: '46px',
+        width: '46px',
+        'font-size': '18pt'
+      })),
+      transition('exited => entered', [
+        animate('100ms')
+      ]),
+      transition('entered => exited', [
+        animate('50ms')
+      ])
+    ])
+  ]
 })
 export class RuneScapeComponent implements OnInit {
 
@@ -13,6 +34,8 @@ export class RuneScapeComponent implements OnInit {
   runeSets = RUNE_SETS;
 
   selectedRune: Rune = null;
+
+  hoveringOver = 0;
 
   constructor() { }
 
